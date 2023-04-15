49876409510ddba508f1649b5506fff519860e78
Chemus14 <75949407+Chemus14@users.noreply.github.com>
2023-04-15 17:44:51 +0100

    Basic CSS folder add - Push Assignment

---
+++ Learning/Hello_world.js
@@ -0,0 +1,12 @@
+var greeting2 = 'Welcome to node.js';
+console.log(greeting2);
+
+var setence1 = 'Learning how to debug code with the debugger';
+console.log(setence1);
+
+var greeting = 'Hello World';
+console.log(greeting);
+var greetingTwo = 'Welcome to node.js';
+console.log(greetingTwo);
+var setence1 = 'Learning how to debug code with the debugger';
+console.log(setence1);
